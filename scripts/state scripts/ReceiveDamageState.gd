718327d00640a1e4d9e994ae0157ea9888b47577
Naitais <enzo.f9849@gmail.com>
2024-12-03 20:51:33 -0300

    aplique metodo que hace daño a la armadura priemro y el resto del daño va a la vida

--- scripts/state scripts/ReceiveDamageState.gd
+++ scripts/state scripts/ReceiveDamageState.gd
@@ -7,10 +7,23 @@ extends State
 signal piece_not_hovered
 
 func receive_damage() -> void:
-	if Global.selected_piece:
-		if Input.is_action_just_pressed("left_click"):
-			actor.health -= Global.selected_piece.physical_damage
-			actor.set_stats()
+	if Global.selected_piece and Input.is_action_just_pressed("left_click"):
+		var damage_taken: int = Global.selected_piece.physical_damage
+		
+		if actor.armor > 0:
+			# Subtract damage from armor first
+			var damage_to_armor = min(damage_taken, actor.armor)  # Only reduce armor by the amount it has
+			actor.armor -= damage_to_armor
+			
+			# Calculate remaining damage after armor
+			var rest_damage = damage_taken - damage_to_armor
+			actor.health -= rest_damage
+		else:
+			# If no armor, apply all damage to health
+			actor.health -= damage_taken
+		
+		actor.set_stats()  # Update stats
+
 			
 func highlight_hovered_piece() -> void:
 	if !actor.isActive:
